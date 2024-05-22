3e25943f6fe133f5f09410c49a2ead34ca82e449
Jiri Kostiha <ijkdata@gmail.com>
2024-05-22 19:32:34 +0200

    feat: trade record constants

---
+++ src/SyncAPIConnector/constants/CategoryName.cs
@@ -0,0 +1,12 @@
+namespace xAPI
+{
+    public static class CategoryName
+    {
+        public const string Forex = "FX";
+        public const string Commodity = "CMD";
+        public const string Etf = "ETF";
+        public const string Stocks = "STC";
+        public const string Indices = "IND";
+        public const string Crypto = "CRT";
+    }
+}
---
+++ src/SyncAPIConnector/constants/GroupName.cs
@@ -0,0 +1,104 @@
+namespace xAPI
+{
+    public static class GroupName
+    {
+        // Predefined groups
+        /// <summary>Major</summary>
+        public const string Major = "Major";
+
+        /// <summary>Minor</summary>
+        public const string Minor = "Minor";
+
+        /// <summary>Emergings</summary>
+        public const string Emergings = "Emergings";
+
+        // Countries
+        /// <summary>United States</summary>
+        public const string US = "US";
+
+        /// <summary>Sweden</summary>
+        public const string Sweden = "Sweden";
+
+        /// <summary>United Kingdom</summary>
+        public const string UK = "UK";
+
+        /// <summary>Spain</summary>
+        public const string Spain = "Spain";
+
+        /// <summary>Germany</summary>
+        public const string Germany = "Germany";
+
+        /// <summary>Poland</summary>
+        public const string Poland = "Poland";
+
+        /// <summary>France</summary>
+        public const string France = "France";
+
+        /// <summary>Belgium</summary>
+        public const string Belgium = "Belgium";
+
+        /// <summary>Finland</summary>
+        public const string Finland = "Finland";
+
+        /// <summary>Italy</summary>
+        public const string Italy = "Italy";
+
+        /// <summary>Denmark</summary>
+        public const string Denmark = "Denmark";
+
+        /// <summary>Netherlands</summary>
+        public const string Netherlands = "Netherlands";
+
+        /// <summary>Portugal</summary>
+        public const string Portugal = "Portugal";
+
+        /// <summary>Czech Republic</summary>
+        public const string CzechRep = "Czech Rep.";
+
+        /// <summary>Switzerland</summary>
+        public const string Switzerland = "Switzerland";
+
+        /// <summary>Norway</summary>
+        public const string Norway = "Norway";
+
+        // Regions
+        /// <summary>Asia-Pacific</summary>
+        public const string AsiaPacific = "Asia-Pacific";
+
+        /// <summary>Europe</summary>
+        public const string Europe = "Europe";
+
+        /// <summary>Americas</summary>
+        public const string Americas = "Americas";
+
+        // Investment types
+        /// <summary>Cryptocurrency</summary>
+        public const string Crypto = "Crypto";
+
+        /// <summary>Exchange Traded Fund</summary>
+        public const string ETF = "ETF";
+
+        /// <summary>Exchange Traded Funds</summary>
+        public const string ETFs = "ETFs";
+
+        // Commodities
+        /// <summary>Precious Metals</summary>
+        public const string PreciousMetals = "Precious Metals";
+
+        /// <summary>Agriculture</summary>
+        public const string Agriculture = "Agriculture";
+
+        /// <summary>Energy</summary>
+        public const string Energy = "Energy";
+
+        /// <summary>Industrial Metals</summary>
+        public const string IndustrialMetals = "Industrial Metals";
+
+        /// <summary>Livestock</summary>
+        public const string Livestock = "Livestock";
+
+        // Other
+        /// <summary>Other</summary>
+        public const string Other = "Other";
+    }
+}
