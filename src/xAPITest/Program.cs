697c3c175fb884de8bad4724d0b8a7536250e83e
Jiri Kostiha <ijkdata@gmail.com>
2024-07-28 20:32:57 +0200

    refactor(example): split sync and async example execution

--- src/xAPITest/Program.cs
+++ src/xAPITest/Program.cs
@@ -3,66 +3,74 @@ using System.Threading;
 using System.Threading.Tasks;
 using xAPI.Sync;
 
-namespace xAPITest
+namespace xAPITest;
+
+internal static class Program
 {
-    internal static class Program
+    private static Server _server = Servers.DEMO;
+    private static string _userId = "16401086";
+    private static string _password = "8Ddddddd";
+
+    private static void Main(string[] args)
     {
-        private static Server _server = Servers.DEMO;
-        private static string _userId = "16401086";
-        private static string _password = "8Ddddddd";
+        RunSyncExample();
+        RunAsyncExample();
 
-        private static void Main(string[] args)
+        Console.WriteLine("Done.");
+        Console.Read();
+    }
+
+    private static void RunSyncExample()
+    {
+        using (var apiConnector = new ApiConnector(_server))
         {
-            using (var apiConnector = new ApiConnector(_server))
-            {
-                Console.WriteLine("----Sync test---");
-                var syncExample = new SyncExample(apiConnector, _userId, _password);
-                syncExample.Run();
-            }
+            Console.WriteLine("----Sync test---");
+            var syncExample = new SyncExample(apiConnector, _userId, _password);
+            syncExample.Run();
+        }
+    }
 
-            using (var apiConnector = new ApiConnector(_server))
-            {
-                Console.WriteLine();
-                Console.WriteLine("----Async test---");
-                Console.WriteLine("(esc) abort");
-                var asyncExample = new AsyncExample(apiConnector, _userId, _password);
-                using var tokenSource = new CancellationTokenSource();
+    private static void RunAsyncExample()
+    {
+        using (var apiConnector = new ApiConnector(_server))
+        {
+            Console.WriteLine();
+            Console.WriteLine("----Async test---");
+            Console.WriteLine("(esc) abort");
+            var asyncExample = new AsyncExample(apiConnector, _userId, _password);
+            using var tokenSource = new CancellationTokenSource();
 
-                var keyWaitTask = Task.Run(() =>
+            var keyWaitTask = Task.Run(() =>
+            {
+                while (!tokenSource.Token.IsCancellationRequested)
                 {
-                    while (!tokenSource.Token.IsCancellationRequested)
+                    if (Console.KeyAvailable)
                     {
-                        if (Console.KeyAvailable)
+                        var key = Console.ReadKey(intercept: true);
+                        if (key.Key == ConsoleKey.Escape)
                         {
-                            var key = Console.ReadKey(intercept: true);
-                            if (key.Key == ConsoleKey.Escape)
-                            {
-                                tokenSource.Cancel();
-                                Console.WriteLine("Operation aborted.");
-                                break;
-                            }
+                            tokenSource.Cancel();
+                            Console.WriteLine("Operation aborted.");
+                            break;
                         }
-                        Thread.Sleep(100);
                     }
-                });
+                    Thread.Sleep(100);
+                }
+            });
 
-                try
-                {
-                    asyncExample.RunAsync(tokenSource.Token).GetAwaiter().GetResult();
-                }
-                catch (OperationCanceledException)
-                {
-                    Console.WriteLine("Operation was canceled.");
-                }
-                finally
-                {
-                    tokenSource.Cancel();
-                    keyWaitTask.Wait();
-                }
+            try
+            {
+                asyncExample.RunAsync(tokenSource.Token).GetAwaiter().GetResult();
             }
-
-            Console.WriteLine("Done.");
-            Console.Read();
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Operation was canceled.");
+            }
+            finally
+            {
+                tokenSource.Cancel();
+                keyWaitTask.Wait();
+            }
         }
     }
 }
