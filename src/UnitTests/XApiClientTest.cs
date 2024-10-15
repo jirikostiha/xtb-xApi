53a1f90444df2f38e5165e4192a8dbf7eb0761a4
Jiri Kostiha <ijkdata@gmail.com>
2024-10-15 10:44:53 +0200

    test: fix - remove additional code

--- src/UnitTests/XApiClientTest.cs
+++ src/UnitTests/XApiClientTest.cs
@@ -1,16 +1,11 @@
-using System.Threading;
-
 namespace Xtb.XApi.UnitTests;
 
 public class XApiClientTest
 {
     [Fact]
-    public async Task Create()
+    public void Create()
     {
         var client = XApiClient.Create("81.2.190.163", 5112, 5113);
-        await client.ConnectAsync();
-        await client.LoginAsync(new Credentials("login", "password"));
-        var openTrades = await client.GetTradesAsync(true);
 
         Assert.NotNull(client.ApiConnector);
         Assert.NotNull(client.ApiConnector.Endpoint);
