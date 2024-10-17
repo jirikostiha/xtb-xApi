8c80dd9235e2634f6c992ce6a1558d7bc705c1a8
Jiri Kostiha <ijkdata@gmail.com>
2024-10-17 23:52:52 +0200

    feat: examples

---
+++ src/Examples/E1.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Xtb.XApi.Examples;
+
+public class E1 : ExampleBase
+{
+    public override async Task Run()
+    {
+        var client = XApiClient.Create("81.2.190.163", 5124, 5125);
+        await client.ConnectAsync();
+        var loginResponse = await client.LoginAsync("16697884", "xoh11724");
+    }
+}
