ef4c528f1d618c0207b05e232640760cd1250deb
Jiri Kostiha <ijkdata@gmail.com>
2024-11-20 14:11:59 +0100

    docs(xml): Extensions ducu

--- src/SyncAPIConnector/abstraction/Extensions.cs
+++ src/SyncAPIConnector/abstraction/Extensions.cs
@@ -5,10 +5,24 @@ namespace Xtb.XApi;
 
 public static class TradeRecordExtensions
 {
+    /// <summary>
+    /// Determines whether the specified trade is a long position.
+    /// </summary>
+    /// <param name="trade">The trade record.</param>
+    /// <returns>
+    /// <c>true</c> if the trade is a long position (BUY, BUY_LIMIT, or BUY_STOP); otherwise, <c>false</c>.
+    /// </returns>
     public static bool IsLongPosition(this ITradeRecord trade) =>
         trade.TradeOperation is not null
         && (trade.TradeOperation == TRADE_OPERATION_TYPE.BUY || trade.TradeOperation == TRADE_OPERATION_TYPE.BUY_LIMIT || trade.TradeOperation == TRADE_OPERATION_TYPE.BUY_STOP);
 
+    /// <summary>
+    /// Determines whether the specified trade is a short position.
+    /// </summary>
+    /// <param name="trade">The trade record.</param>
+    /// <returns>
+    /// <c>true</c> if the trade is a short position (SELL, SELL_LIMIT, or SELL_STOP); otherwise, <c>false</c>.
+    /// </returns>
     public static bool IsShortPosition(this ITradeRecord trade) =>
         trade.TradeOperation is not null
         && (trade.TradeOperation == TRADE_OPERATION_TYPE.SELL || trade.TradeOperation == TRADE_OPERATION_TYPE.SELL_LIMIT || trade.TradeOperation == TRADE_OPERATION_TYPE.SELL_STOP);
