9993c5e2d39011002f790c4dab6a5fc5126ddcfc
Jiri Kostiha <ijkdata@gmail.com>
2024-11-25 10:01:23 +0100

    fix: building with netstandard2.1

--- src/SyncAPIConnector/Connector.cs
+++ src/SyncAPIConnector/Connector.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
