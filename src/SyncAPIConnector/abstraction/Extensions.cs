fcd3af18e223466afc916667e6fcf0a38ddbe0a5
Jiri Kostiha <ijkdata@gmail.com>
2025-02-20 05:19:28 +0100

    refactor: remove IsCfdStock - not correct

--- src/SyncAPIConnector/abstraction/Extensions.cs
+++ src/SyncAPIConnector/abstraction/Extensions.cs
@@ -40,13 +40,6 @@ public static class TradeRecordExtensions
         && (trade.TradeOperation == TRADE_OPERATION_TYPE.SELL || trade.TradeOperation == TRADE_OPERATION_TYPE.SELL_LIMIT || trade.TradeOperation == TRADE_OPERATION_TYPE.SELL_STOP);
 
     /// <summary>
-    /// Indicates if market is cfd stock market.
-    /// It is based on symbol name as xtb is using it to distinguish between stocks and cfd stocks.
-    /// </summary>
-    public static bool IsCfdStock(this IHasSymbol symbol) =>
-        symbol?.Symbol is not null && symbol.Symbol.EndsWith("_4", StringComparison.InvariantCulture);
-
-    /// <summary>
     /// Net profit in account currency. It is gross profit minus fees for storage (swap + rollover)
     /// </summary>
     public static double? NetProfit(this ITradeRecord trade) =>
